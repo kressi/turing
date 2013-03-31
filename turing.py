b98df77bd6b700b5e17ae0ec232e006ab0651b7d
michael <michael@notebook.sitecomwl351>
2013-03-31 17:16:32 +0200

    Initial upload

---
+++ turing.py
@@ -0,0 +1,60 @@
+from turing_machine import TuringMachine
+import sys, ast, os
+
+
+# ********************************
+# prepare config 
+# ********************************
+config = {}
+
+#default values
+config["tapeCount"] = 2
+config["transitions"] = {'0-00':'0-00RN', '0-#0':'0-00RR'}
+config["tapesInput"] = ["00000", "000000"]
+
+def writeConfig(handle):
+    # write config file from stdin
+    config = {}
+    config["tapeCount"] = int(input("number of tapes? "))
+#    config["initState"] = 1
+    config["transitions"] = {}
+    if raw_input("from file? Y n ") == "n":
+        trans_string = raw_input("transitions? ")
+    else:
+        with open(raw_input("filename? "),'r') as trans_f:
+            trans_string = trans_f.read()
+    for trans in trans_string.split(","):
+        parts = trans.split(":")
+        config["transitions"][parts[0]] = parts[1]
+    if config:
+        handle.write(str(config))
+
+if len(sys.argv) > 1:
+    if sys.argv[1] + ".conf" in os.listdir(os.getcwd()):
+        # read config file
+        with open(sys.argv[1] + ".conf", 'r') as config_f:
+            config = ast.literal_eval(config_f.read())
+    else:
+        with open(sys.argv[1] + ".conf", 'w') as config_f:
+            writeConfig(config_f)
+    if len(sys.argv) > 2:
+        # read tapes input from argument values
+        config["tapesInput"] = []
+        for i in range(2,len(sys.argv)):
+            config["tapesInput"].append(sys.argv[i])
+    else:
+        # read tapes input from stdin
+        config["tapesInput"] = str(raw_input("content of tapes? ")).split(" ")
+
+
+
+# ********************************
+# run turing machine
+# ********************************
+t = TuringMachine(**config)
+t.show()
+while t.step():
+    # next step with enter
+    #raw_input()
+    t.show()
+
---
+++ turing_machine.py
@@ -0,0 +1,92 @@
+class Config:
+    blank = "#"
+
+class Tape:
+    """Initial position on the leftmost element."""
+    def __init__(self, inputTape = ""):
+        self.__frontStack = []
+        self.__endStack = []
+        for s in inputTape:
+            self.__endStack.append(s)
+        self.__endStack.reverse()
+        if self.__endStack:
+            self.__current = self.__endStack.pop()
+        else:
+            self.__current = Config.blank
+    def current(self, *s):
+        if s:
+            self.__current = s[0]
+        return self.__current
+    def left(self, s):
+        if self.__endStack or s != Config.blank:
+            self.__endStack.append(s)
+        if self.__frontStack:
+            self.__current = self.__frontStack.pop()
+        else:
+            self.__current = Config.blank
+        return self.__current
+    def right(self, s):
+        if self.__frontStack or s != Config.blank:
+            self.__frontStack.append(s)
+        if self.__endStack:
+            self.__current = self.__endStack.pop()
+        else:
+            self.__current = Config.blank
+        return self.__current
+    def show(self):
+        return " ".join(self.__frontStack) + "[" + self.__current + "]" +(" ".join(self.__endStack)[::-1])
+
+
+class TuringMachine:
+    def __init__(self, tapesInput = [""], initState = 0, transitions = {}, tapeCount = 1):
+        self.__state = initState
+        self.__transitions = transitions
+        self.__stepCount = 0L
+        self.__next = ""
+        self.__tapes = []
+        for t in tapesInput:
+            self.__tapes.append(Tape(t))
+        if tapeCount > len(self.__tapes):
+            for i in range(len(self.__tapes), tapeCount):
+                self.__tapes.append(Tape())
+
+    def show(self):
+        print "state: {s}  step: {c}".format(s = self.__state, c = self.__stepCount)
+        for t in self.__tapes:
+            print t.show()
+
+    def step(self):
+        if self.__next == "":
+            self.__next = str(self.__state) + "-"
+            for t in self.__tapes:
+                self.__next += t.current()
+        if self.__next in self.__transitions:
+            self.__stepCount += 1
+            trans = self.__transitions[self.__next].split("-")
+            self.__state = trans[0]
+            self.__next = self.__state + "-"
+            values = trans[1][:len(trans[1])/2]
+            directions = trans[1][len(trans[1])/2:]
+            for i in range(len(values)):
+                if directions[i] == "R":
+                    self.__next += self.__tapes[i].right(values[i])
+                elif directions[i] == "L":
+                    self.__next += self.__tapes[i].left(values[i])
+                else:
+                    self.__next += self.__tapes[i].current(values[i])
+            return True
+                
+        else:
+            return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
